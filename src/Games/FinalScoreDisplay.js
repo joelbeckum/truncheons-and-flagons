6ae343ea9c0a427f7ed748d2490ea4e442b9732c
Sam Carbine <scarbine@me.com>
2021-06-18 15:49:50 -0500

    fixed and wrong link causing bug

--- src/Games/FinalScoreDisplay.js
+++ src/Games/FinalScoreDisplay.js
@@ -1,25 +1,19 @@
-import {startGame} from "./startGame.js" 
+import { startGamePage } from "./startGame.js";
 
-
- export const FinalScoreDisplay = () => {
-	return `
+export const FinalScoreDisplay = () => {
+  return `
 		<section> 
 			<div> The {Team Name Place Holder} has won the game! Roar! </div>
-
-			
 			<button id="end_game" class="end_game">Start Game</button>
-			<button id="submit_new_team"> Add New Team </button>
-			</div>
-
 		</section>
-	`
- }
+	`;
+};
 
- const mainContainer = document.querySelector(".container")
+const mainContainer = document.querySelector(".container");
 
-document.addEventListener("click",(event) => {
-	if (event.target.id === "end_game"){
-		mainContainer.innerHTML = startGame()
-		console.log("submit new player is connected")
-	}
-} )
+document.addEventListener("click", (event) => {
+  if (event.target.id === "end_game") {
+    mainContainer.innerHTML = startGamePage();
+    console.log("submit new player is connected");
+  }
+});
