9c556f75a1042e6c6c301dbee29d91aa72cd7426
Sam Carbine <scarbine@me.com>
2021-06-18 17:15:26 -0500

    Added some logic for the basic game paly and counters to keep the running score

--- src/Games/EnterGameScore.js
+++ src/Games/EnterGameScore.js
@@ -5,12 +5,11 @@ export const EnterGameScores = () => {
 	 <section>
 	 	<form >
 			<label for="team_1_score">{Team 1 Name Placeholder}</label>
-			<input type="number" id="team_1_score" name="team_1_score"><br><br>
+			<input  type="number" id="team_1_score" name="team_1_score"><br><br>
 			<label for="team_2_score">{Team 2 Name Placeholder}</label>
-			<input type="number" id="team_2_score" name="team_2_score"><br><br>
+			<input  type="number" id="team_2_score" name="team_2_score"><br><br>
 			<label for="team_3_score">{Team 3 Name Placeholder}</label>
-			<input type="number" id="team_3_score" name="team_3_score"><br><br>
-		
+			<input 	 type="number" id="team_3_score" name="team_3_score"><br><br>	
 		</form>
 		<button id="submit_round_score"> Submit Score </button>
 	 
@@ -18,11 +17,54 @@ export const EnterGameScores = () => {
 	 `;
 };
 
-const mainContainer = document.querySelector(".container")
+const mainContainer = document.querySelector(".container");
+let round = 1;
 
-document.addEventListener("click",(event) => {
-	if (event.target.id === "submit_round_score"){
-		mainContainer.innerHTML = FinalScoreDisplay()
-		console.log("submit new player is connected")
-	}
-} )
+let totalTeam1Score = 0;
+let totalTeam2Score = 0;
+let totalTeam3Score = 0;
+
+document.addEventListener("click", (event) => {
+  if (event.target.id === "submit_round_score") {
+    if (round < 3) {
+      let teamScore1 = parseInt(document.getElementById("team_1_score").value);
+      let teamScore2 = parseInt(document.getElementById("team_2_score").value);
+      let teamScore3 = parseInt(document.getElementById("team_3_score").value);
+      totalTeam1Score += teamScore1;
+      totalTeam2Score += teamScore2;
+      totalTeam3Score += teamScore3;
+      round++;
+
+      console.log(
+        "submit new player is connected",
+        teamScore1,
+        teamScore2,
+        teamScore3,
+        totalTeam1Score,
+        totalTeam2Score,
+        totalTeam3Score
+      );
+
+      mainContainer.innerHTML = EnterGameScores();
+    } else {
+      let teamScore1 = parseInt(document.getElementById("team_1_score").value);
+      let teamScore2 = parseInt(document.getElementById("team_2_score").value);
+      let teamScore3 = parseInt(document.getElementById("team_3_score").value);
+      totalTeam1Score += teamScore1;
+      totalTeam2Score += teamScore2;
+      totalTeam3Score += teamScore3;
+
+      console.log("the final score is :" ,totalTeam1Score, totalTeam2Score, totalTeam3Score);
+
+      // need to push scores to the database here before they get reset for them new game
+      
+      mainContainer.innerHTML = FinalScoreDisplay();
+
+      round = 1;
+      totalTeam1Score = 0;
+      totalTeam2Score = 0;
+      totalTeam3Score = 0;
+
+    }
+  }
+});
--- src/Games/games.js
+++ src/Games/games.js
@@ -1,21 +1,24 @@
 import { EnterGameScores } from "./EnterGameScore.js";
+import { getTeams} from "../dataAccess.js"
 
 // generate the html for adding choose_teams dropdown that is called from the click event listener of the start game button
 export const ChooseTeams = () => {
+
+	const teams = getTeams()
   return `
 	<section class="choose_teams"> 
 		<form >
-			<label for="choose_team">Choose Team 1:</label>
-			<select name="player_team_picker" id="player_team_picker">
-			<option value="{teamId}">Map the teams and filter based on who is not full.</option>  
+			<label>Choose Team 1:</label>
+			<select name="player_team_picker" id="player_team_picker_1">
+			${teams.map((team) => `<option value="${team.id}">${team.teamName}</option>`).join("") }
 			</select><br><br>	
-			<label for="choose_team">Choose Team 2:</label>
-			<select name="player_team_picker" id="player_team_picker">
-			<option value="{teamId}">Map the teams and filter based on who is not full.</option>  
+			<label>Choose Team 2:</label>
+			<select name="player_team_picker" id="player_team_picker_2">
+			${teams.map((team) => `<option value="${team.id}">${team.teamName}</option>`).join("") }  
 			</select><br><br>	
-			<label for="choose_team">Choose Team 3:</label>
-			<select name="player_team_picker" id="player_team_picker">
-			<option value="{teamId}">Map the teams and filter based on who is not full.</option>  
+			<label>Choose Team 3:</label>
+			<select name="player_team_picker" id="player_team_picker_3">
+			${teams.map((team) => `<option value="${team.id}">${team.teamName}</option>`).join("") } 
 			</select><br><br>	
 		</form>
 		<button id="submit_chosen_teams"> Choose Teams </button>
@@ -27,6 +30,20 @@ const mainContainer = document.querySelector(".container");
 
 document.addEventListener("click", (event) => {
   if (event.target.id === "submit_chosen_teams") {
+	let totalTeam1Score = 0
+	let totalTeam2Score = 0
+	let totalTeam3Score = 0
+
+	const team1 = document.getElementById("player_team_picker_1").value
+	const team2 = document.getElementById("player_team_picker_2").value
+	const team3 = document.getElementById("player_team_picker_3").value
+
+	totalTeam1Score += team1
+
+	console.log(team1, team2, team3, )
+
     mainContainer.innerHTML = EnterGameScores();
+
+    
   }
 });
