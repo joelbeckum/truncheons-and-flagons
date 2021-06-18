d8fc4b995e32b5472332ac20307484dfe157a34c
Sam Carbine <scarbine@me.com>
2021-06-18 11:52:47 -0500

    added basic event listener to the buttons

--- src/Games/startGame.js
+++ src/Games/startGame.js
@@ -1,3 +1,9 @@
 export const startGamePage = () => {
-	return '<div class="button_wrapper_start"><button class="start_game_button">Start Game</button></div>'
+	return '<div class="button_wrapper_start"><button id="start_game" class="start_game_button">Start Game</button></div>'
 }
+
+document.addEventListener("click",(event) => {
+	if (event.target.id === "start_game"){
+		console.log("The start game button is connected")
+	}
+} )
