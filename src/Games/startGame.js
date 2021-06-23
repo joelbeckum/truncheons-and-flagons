c1f762930ad8449fbe52b4e70c7f8481feeeb588
Sam Carbine <scarbine@me.com>
2021-06-23 14:49:00 -0500

    added dragon

--- src/Games/startGame.js
+++ src/Games/startGame.js
@@ -1,5 +1,5 @@
 import { ChooseTeams } from "./games.js";
-import { applicationState} from "../dataAccess.js"
+import { applicationState } from "../dataAccess.js";
 
 export const startGamePage = () => {
   return '<div class="button_wrapper_start"><button id="start_game" class="start_game_button">Start Game</button></div>';
@@ -11,6 +11,9 @@ document.addEventListener("click", (event) => {
   if (event.target.id === "start_game") {
     applicationState.isPlaying.isPlaying = true;
 
-    mainContainer.innerHTML = ChooseTeams();
+    mainContainer.innerHTML = `<div class="dragon_fire"><iframe src="https://giphy.com/embed/TfAHHhLYRTlScWkhdJ" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/larianstudios-dragon-baldurs-gate-3-bg3-TfAHHhLYRTlScWkhdJ"></a></p>`;
+    setTimeout(() => {
+      mainContainer.innerHTML = ChooseTeams();
+    }, 3250);
   }
 });
